06ed65c324dd6d33ee471dc2f99f7eddd86d4e95
Reggie Cushing <reggie.cushing@dans.knaw.nl>
2026-07-03 09:14:02 +0200

    Updated R installation packages to include tidyverse, rmarkdown, httr, shin[4D[K shinydashboard, and leaflet for improved project functionality.

--- install.R
+++ install.R
@@ -1,13 +1,5 @@
-install.packages(c(
-  "IRkernel",
-  "tidyverse",
-  "data.table",
-  "jsonlite",
-  "readr",
-  "dplyr",
-  "ggplot2",
-  "dotenv",
-  "remotes"
-))
-
-IRkernel::installspec(user = FALSE)
+install.packages("tidyverse")
+install.packages("rmarkdown")
+install.packages("httr")
+install.packages("shinydashboard")
+install.packages("leaflet")
